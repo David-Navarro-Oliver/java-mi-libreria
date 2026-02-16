6209789738e3b339063c999729382cf25fc96f21
David-Navarro-Oliver <navarropalle@gmail.com>
2026-02-16 21:30:45 +0100

    feat: add Book class with attributes and getters/setters

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,71 @@
+package org.example;
+
+public class Book {
+    private String title;
+    private String description;
+    private String isbn;
+    private String writer;
+    private double price;
+
+    public Book() {
+    }
+
+    public Book(String title, String description, String isbn, String writer, double price) {
+        this.title = title;
+        this.description = description;
+        this.isbn = isbn;
+        this.writer = writer;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", writer='" + writer + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
