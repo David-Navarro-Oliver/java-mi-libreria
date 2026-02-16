3eee1e47f2d4f2a312a0dfc3ea0bceafae9f08c6
David-Navarro-Oliver <navarropalle@gmail.com>
2026-02-16 21:34:19 +0100

    feat: create and print 5 books

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,53 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+    static void main(String[] args) {
+
+        Book book1 = new Book(
+                "Clean Code",
+                "A handbook of agile software craftsmanship",
+                "9780132350884",
+                "Robert C. Martin",
+                35.99
+        );
+
+        Book book2 = new Book(
+                "Effective Java",
+                "Best practices for the Java platform",
+                "9780134685991",
+                "Joshua Bloch",
+                42.50
+        );
+
+        Book book3 = new Book(
+                "Design Patterns",
+                "Elements of reusable object-oriented software",
+                "9780201633610",
+                "Erich Gamma",
+                50.00
+        );
+
+        Book book4 = new Book(
+                "Refactoring",
+                "Improving the design of existing code",
+                "9780201485677",
+                "Martin Fowler",
+                47.75
+        );
+
+        Book book5 = new Book(
+                "The Pragmatic Programmer",
+                "Your journey to mastery",
+                "9780201616224",
+                "Andrew Hunt",
+                39.90
+        );
+
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+        System.out.println(book4);
+        System.out.println(book5);
     }
 }
